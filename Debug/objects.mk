154559daf2dd42f43e6065218f74d077247d3f97
Daniel Walnut <me@danielwalnut.com>
2018-03-02 15:57:08 -0800

    Forked TI pwmled with TIRTOS example as a starting point. Also added the GCC build folder, changed the makefile and kept Windows CCS config files

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l:"ti/display/lib/display.am4g" -l:"ti/drivers/lib/drivers_cc32xx.am4g" -l:"third_party/fatfs/lib/fatfs.am4g" -l:"ti/dpl/lib/dpl_cc32xx.am4g" -l:"ti/devices/cc32xx/driverlib/gcc/Release/driverlib.a" -lgcc -lm -lnosys -lc
+
