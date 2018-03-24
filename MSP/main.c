2b72ffb12e7fbc3d87980b6ab31a6d3288c29c00
Daniel Walnut <me@danielwalnut.com>
2018-03-23 20:17:08 -0700

    Added start stop functionality, disabled UART, and made setpoint slower

--- MSP/main.c
+++ MSP/main.c
@@ -19,6 +19,7 @@ by Daniel Walnut and Tim Yue
 #include "Scheduler/Scheduler.h"
 #include "LineCruiser/LineCruiser.h"
 #include "UARTIO/UARTIO.h"
+#include "StartStop/StartStop.h"
 
 // Includes just for diagnostics purposes
 #include "LineCruiser/DiffDriver/DiffDriver.h"
@@ -36,18 +37,20 @@ int main(void)
 	lineCruiserInit();		// Initialize the line cruiser.
 	UARTIOInit(); 			// Initialize communication with Computer Console
 	
+	setupStartStop();
+	
 	// Let's get this party started!
 	__enable_interrupt(); 	// Enable global interrupts. Everything must be configured before this.
 	
-	lineCruise(100); // Let's cruise at 60cm/s
+	lineCruise(30); // Let's cruise at 30cm/s
 	
 	while(1) {
 		//Nothing yet
 		char LeString[150];
 		int strSize;
 		LSRead(); // CANNOT BE IN SCHEDULER BECAUSE IT NEEDS GIE TO WORK.
-		strSize = sprintf(LeString, "sensor: %d | 1: %d RPM | 2: %d RPM | S: %d cm/s | R: %d cm\n\r", (int) (lastSensorPosition*100), (int) getRPM(1), (int) getRPM(2), (int) getSpeed(), (int) getCurveRadius());
-		UARTIOSend(LeString, strSize);
+		//strSize = sprintf(LeString, "sensor: %d | 1: %d RPM | 2: %d RPM | S: %d cm/s | R: %d cm\n\r", (int) (lastSensorPosition*100), (int) getRPM(1), (int) getRPM(2), (int) getSpeed(), (int) getCurveRadius());
+		//UARTIOSend(LeString, strSize);		
 		__delay_cycles(1600000);
 	}
 	return 0;
