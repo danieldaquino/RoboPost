bfff5f1920718bc24f7dac7b777aa407659997a1
Daniel Walnut <me@danielwalnut.com>
2018-03-24 23:22:40 -0700

    Optimization phase 1, Test 3

--- MSP/LineCruiser/LineCruiser.h
+++ MSP/LineCruiser/LineCruiser.h
@@ -40,8 +40,8 @@ Macros
 // Tightest curve radius in cm
 #define SHARPEST_CURVE 15
 // CRUISE_KP + CRUISE_KD = 1
-#define CRUISE_KD 0.8
-#define CRUISE_KP 0.2
+#define CRUISE_KD 0.7
+#define CRUISE_KP 0.3
 // Number between 0 and 1 to tell how much to slow down in tightening curves. 0 means never slows down. 1 means full stop on tightest curve
 #define CORNERING_D_BRAKE_FACTOR 0.9
 // Number between 0 and 1 to tell how much to slow down in any curve. 0 means never slows down. 1 means full stop on tightest curve
