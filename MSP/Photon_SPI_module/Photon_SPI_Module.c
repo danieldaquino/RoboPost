0ba9d0af7a23882daabb30b0af7c64e5de3d5112
Daniel Walnut <me@danielwalnut.com>
2018-04-02 15:58:13 -0700

    Moved ucsiSpi module to its own folder, fixed some indentation issues and created blank header file

--- MSP/Photon_SPI_module/Photon_SPI_Module.c
+++ MSP/Photon_SPI_module/Photon_SPI_Module.c
@@ -3,20 +3,19 @@
 #include <stdint.h>
 
 #define NUM_PARAM   11
+
 unsigned char InfoBoardUpdate(int *DataArray,int *CommandArray);
 void InfoBoardInit(int *DataArray,int *CommandArray);
+
 unsigned char InfoBoardUpdate(int *DataArray,int *CommandArray)
 {
+	P6OUT&=~BIT5;       // enable SS
+	__delay_cycles(50);
+	spiTx_RXByte(DataArray,CommandArray,NUM_PARAM);// transmit whats in dataArray and put received data in CommandArray
 
-
-        P6OUT&=~BIT5;       // enable SS
-                __delay_cycles(50);
-                spiTx_RXByte(DataArray,CommandArray,NUM_PARAM);// transmit whats in dataArray and put received data in CommandArray
-
-                   // __delay_cycles(160);
-                    P6OUT|=BIT5;    // disable SS
-                    __delay_cycles(200);  // wait for photon to process byte
-
+	// __delay_cycles(160);
+	P6OUT|=BIT5;    // disable SS
+	__delay_cycles(200);  // wait for photon to process byte
 }
 
 void InfoBoardInit(int *DataArray,int *CommandArray)
