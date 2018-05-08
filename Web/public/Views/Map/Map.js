0a38964728309625e8bbc7430e090b2d797768f8
Daniel Walnut <me@danielwalnut.com>
2018-05-08 02:31:48 -0700

    Drag and drop logic complete

--- Web/public/Views/Map/Map.js
+++ Web/public/Views/Map/Map.js
@@ -32,7 +32,19 @@ function Map(MapContainer, MapButton) {
 	that.DropZones[0].style.left = "27.5%";
 	that.DropZones[0].style.width = "25.5%";
 	that.DropZones[0].style.height = "12%";
-	that.DropZones[0].style.borderColor = "#44F587";
+	that.DropZones[0].addEventListener('dragover', function(e) {
+		e.preventDefault();
+		that.DropZones[0].className = "DropZone DragOver"
+		e.dataTransfer.dropEffect = "move";
+	});
+	that.DropZones[0].addEventListener('dragleave', function() {
+		that.DropZones[0].className = "DropZone Active"
+	});
+	that.DropZones[0].addEventListener('drop', function(e) {
+		// DROP
+		var TheID = e.dataTransfer.getData("TheID");
+		that.DropZones[0].appendChild(document.getElementById(TheID));
+	});
 	that.Map.appendChild(that.DropZones[0]);
 	
 	that.DropZones[1] = document.createElement("div");
@@ -41,7 +53,19 @@ function Map(MapContainer, MapButton) {
 	that.DropZones[1].style.left = "27.5%";
 	that.DropZones[1].style.width = "20%";
 	that.DropZones[1].style.height = "12%";
-	that.DropZones[1].style.borderColor = "#44F587";
+	that.DropZones[1].addEventListener('dragover', function(e) {
+		e.preventDefault();
+		that.DropZones[1].className = "DropZone DragOver"
+		e.dataTransfer.dropEffect = "move";
+	});
+	that.DropZones[1].addEventListener('dragleave', function() {
+		that.DropZones[1].className = "DropZone Active"
+	});
+	that.DropZones[1].addEventListener('drop', function(e) {
+		// DROP
+		var TheID = e.dataTransfer.getData("TheID");
+		that.DropZones[1].appendChild(document.getElementById(TheID));
+	});
 	that.Map.appendChild(that.DropZones[1]);
 	
 	that.DropZones[2] = document.createElement("div");
@@ -50,7 +74,19 @@ function Map(MapContainer, MapButton) {
 	that.DropZones[2].style.left = "49.8%";
 	that.DropZones[2].style.width = "6%";
 	that.DropZones[2].style.height = "26%";
-	that.DropZones[2].style.borderColor = "#44F587";
+	that.DropZones[2].addEventListener('dragover', function(e) {
+		e.preventDefault();
+		that.DropZones[2].className = "DropZone DragOver"
+		e.dataTransfer.dropEffect = "move";
+	});
+	that.DropZones[2].addEventListener('dragleave', function() {
+		that.DropZones[2].className = "DropZone Active"
+	});
+	that.DropZones[2].addEventListener('drop', function(e) {
+		// DROP
+		var TheID = e.dataTransfer.getData("TheID");
+		that.DropZones[2].appendChild(document.getElementById(TheID));
+	});
 	that.Map.appendChild(that.DropZones[2]);
 	
 	that.DropZones[3] = document.createElement("div");
@@ -59,33 +95,57 @@ function Map(MapContainer, MapButton) {
 	that.DropZones[3].style.left = "74%";
 	that.DropZones[3].style.width = "6%";
 	that.DropZones[3].style.height = "26%";
-	that.DropZones[3].style.borderColor = "#44F587";
+	that.DropZones[3].addEventListener('dragover', function(e) {
+		e.preventDefault();
+		that.DropZones[3].className = "DropZone DragOver"
+		e.dataTransfer.dropEffect = "move";
+	});
+	that.DropZones[3].addEventListener('dragleave', function() {
+		that.DropZones[3].className = "DropZone Active"
+	});
+	that.DropZones[3].addEventListener('drop', function(e) {
+		// DROP
+		var TheID = e.dataTransfer.getData("TheID");
+		that.DropZones[3].appendChild(document.getElementById(TheID));
+	});
+
 	that.Map.appendChild(that.DropZones[3]);
 	
 	//Create draggables
 	that.RedStation = document.createElement("div");
 	that.RedStation.className = "StationCircle";
+	that.RedStation.id = "MapRedStation";
 	that.RedStation.style.background = DriftingRed;
 	that.RedStation.draggable = true;
 	that.RedStation.setAttribute('draggable', true);
 	that.RedStation.addEventListener('dragstart', function(e) {
 		e.dataTransfer.effectAllowed = 'move';
-		e.dataTransfer.setData('Text', this.id);
+		e.dataTransfer.setData('TheID', this.id);
 		this.style.opacity = 0.4;
+		that.ShowAllDropZones();
+	}, false);
+	that.RedStation.addEventListener('dragend', function(e) {
+		this.style.opacity = 1;
+		that.HideAllDropZones();
 	}, false);
 	that.Map.appendChild(that.RedStation);
 	
 	that.BlueStation = document.createElement("div");
 	that.BlueStation.className = "StationCircle";
+	that.BlueStation.id = "MapBlueStation";
 	that.BlueStation.style.background = SettleBlue;
 	that.BlueStation.setAttribute('draggable', true);
 	that.BlueStation.addEventListener('dragstart', function(e) {
 		e.dataTransfer.effectAllowed = 'move';
-		e.dataTransfer.setData('Text', this.id);
+		e.dataTransfer.setData('TheID', this.id);
 		this.style.opacity = 0.4;
+		that.ShowAllDropZones();
+	}, false);
+	that.BlueStation.addEventListener('dragend', function(e) {
+		this.style.opacity = 1;
+		that.HideAllDropZones();
 	}, false);
 	that.Map.appendChild(that.BlueStation);
-	
 	
 	that.MapOpen = false;
 	
@@ -114,4 +174,17 @@ function Map(MapContainer, MapButton) {
 			that.Map.className = "MapBox GlueX";
 		}
 	}
+	
+	that.ShowAllDropZones = function() {
+		for(var i=0;i < that.DropZones.length;i++) {
+			that.DropZones[i].className = "DropZone Active";
+		}
+	}
+	
+	that.HideAllDropZones = function() {
+		for(var i=0;i < that.DropZones.length;i++) {
+			that.DropZones[i].className = "DropZone";
+		}
+	}
+
 }
